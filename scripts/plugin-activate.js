50fb2c311160a6c9b36c100814543c71288b2b05
sakthivel <sakthivelct21@gmail.com>
2016-12-26 12:12:59 +0530

    updated file

---
+++ scripts/plugin-activate.js
@@ -0,0 +1,8 @@
+/*DL Menu plugin start here*/
+$(document).ready(function() {
+    $('#dl-menu').dlmenu({
+        animationClasses: { in : 'dl-animate-in-2',
+            out: 'dl-animate-out-2'
+        }
+    });    
+});
